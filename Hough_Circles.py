b0f4c59c3b8fa955096f554e336d93544e9811b7
Madera <MaderaHLN@hotmail.com>
2021-09-15 12:11:27 -0500

    Limpieza el archivo .py de comentarios y el anexo del video hoja2

--- Hough_Circles.py
+++ Hough_Circles.py
@@ -1,7 +1,5 @@
 import cv2 
 import numpy as np
-import time 
-from time import time
 
 #Cálculo de distancias
 def distancia(puntos, img):
@@ -13,7 +11,7 @@ def distancia(puntos, img):
         if x1 < x2:
             distancia_pixeles = abs(x2 - x1) #Cálculo de dsitancia en pixeles
             distancia_cm = (distancia_pixeles)/21.5 #Cálculo de distancia en cm
-            cv2.putText(img, "{:.2f} cm".format(distancia_cm), (x1+w1+distancia_pixeles//2, y1-30), 2, 0.8, (0,0,255), 1, cv2.LINE_AA)
+            cv2.putText(img, "{:.2f} cm".format(distancia_cm), (x1+w1+distancia_pixeles//2, y1-30), 2, 0.8, (0,0,0), 1, cv2.LINE_AA)
             cv2.line(img,(x1,y1-20),(x2, y1-20),(0, 0, 255),2) #Línea horizontal
             cv2.line(img,(x1,y1-30),(x1, y1-10),(0, 0, 255),2)
             cv2.line(img,(x2,y1-30),(x2, y1-10),(0, 0, 255),2)
@@ -22,21 +20,17 @@ def distancia(puntos, img):
         else:
             distancia_pixeles = abs(x1 - x2) #Cálculo de dsitancia en pixeles
             distancia_cm = (distancia_pixeles)/21.5 #Cálculo de distancia en cm
-            cv2.putText(img, "{:.2f} cm".format(distancia_cm), (x2+w2+distancia_pixeles//2, y2-30), 2, 0.8, (0,0,255), 1, cv2.LINE_AA)
+            cv2.putText(img, "{:.2f} cm".format(distancia_cm), (x2+w2+distancia_pixeles//2, y2-30), 2, 0.8, (0,0,0), 1, cv2.LINE_AA)
             cv2.line(img,(x2,y2-20),(x1, y2-20),(0, 0, 255),2) #Línea horizontal
             cv2.line(img,(x2,y2-30),(x2, y2-10),(0, 0, 255),2) #Línea izquierda
             cv2.line(img,(x1,y2-30),(x1, y2-10),(0, 0, 255),2) #Línea derecha
             cv2.imshow('img',img)
-            #cv2.waitKey(0)
-            #cv2.imshow('frame',frame)   
-        #k = cv2.waitKey(1) & 0xFF
-        #if k == 27:
-        #    break
     else:
         print("Se han detectado 1 o más de 2 circulos")
-        print("Verifique que el lugar este bien iluminado")
+        #print("Verifique que el lugar este bien iluminado") #Mensaje extra cuando se trata de cámara en tiempo real
 
 def ordenar_puntos(puntos):
+    
     n_puntos = np.concatenate([puntos[0], puntos[1], puntos[2], puntos[3]]).tolist()
     y_order = sorted(n_puntos, key=lambda n_puntos: n_puntos[1])
     x1_order = y_order[:2]
@@ -66,37 +60,17 @@ def roi(image, ancho, alto):
     return imagen_alineada
 
 def main(cap):
-    # Imagen original
-    #cap = cv2.imread('cir1.jpg', cv2.IMREAD_COLOR)
-    #ret, frame = cap.read()
     img = roi(cap, ancho=600, alto=465)
-    #cv2.imshow('Original', img)
-    #cv2.waitKey(0)
-
     # Convertir a escala de grises
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
-    #cv2.imshow('Grigio', gray) # grigio es en italiano, pero no se asusten, no tiene Coreanovirus
-    #cv2.waitKey(0)
-
+    cv2.imshow('Gris', gray)
     # Ahora se aplica un filtro pasabajas de 3x3
     gray_blurred = cv2.blur(gray, (11, 11)) 
-    #cv2.imshow('Borrosa', gray_blurred )
-    #cv2.waitKey(0)
-
-    # Aplicar la tranfromada de Hough para detección de círculos
-    # parámetros:
-    # imagen a la cual se le va a aplicar
-    # método de detección a realizar, por lo general se utiliza HOUGH_GRADIENT
-    # relación inversa de la resolución del acumulador, si no se gusta meter en líos, lo ideal es usar 1, pues se tiene la misma resolución que la imagen
-    # diatancia mínima (en pixeles) entre el centro y las circunferencias detectadas
-    # param1, En el caso de utilizar el método HOUGH_GRADIENT, es el umbral máximo en la detección de bordes por Canny
-    # param2, En el caso de utilizar el método HOUGH_GRADIENT, es el umbral mínimo en la detección de bordes por Canny
-    # minRadius, es el radio mínimo del círculo (no se interpone con la distancia mínima entre el centro y la circunferencia)
-    # maxRadius, es el radio mínimo del círculo (no se interpone con la distancia mínima entre el centro y la circunferencia)
+    cv2.imshow('Borrosa', gray_blurred )
     detected_circles = cv2.HoughCircles(gray_blurred, cv2.HOUGH_GRADIENT, 1, 15, param1 = 20, param2 = 30, minRadius = 30, maxRadius = 80) 
     # Revisar que el método haya regresado algún valor
     if detected_circles is not None: 
-        # Convertir los parámetros el círculo a, b, y r en enteros de 16 bits
+        # Convertir los parámetros el círculo a, b, y r en enteros
         detected_circles = np.uint16(np.around(detected_circles))
         puntos = []
         # Ahora si se recorren todos los círculos detectados
@@ -107,36 +81,26 @@ def main(cap):
             puntos.append(datos)
             cv2.circle(img, (a, b), r, (0, 255, 0), 2) 
             # Mostrar los datos de las circunferencias
-            print("Centro ({:}, {:}), radio = {:}".format(a, b, r))
+            #print("Centro ({:}, {:}), radio = {:}".format(a, b, r))
             # Dibujar un círculo pequeño alrededor del centro
             cv2.circle(img, (a, b), 1, (0, 0, 255), 3)
             # Ir mostradndo las circunferencias detectadas
-            cv2.imshow("Detección de circunferencias", img) 
-            #cv2.waitKey(0)
-        
+            cv2.imshow("Circunferencia encontradas", img)         
         distancia(puntos, img)
-
-#Llamada para cálculo de distancia
 
 cap = cv2.VideoCapture('hoja2.mp4')
 #cap = cv2.VideoCapture(0)
 #cap.release()
-#img = frame
+
 while True:
     ret, frame = cap.read()
-    if ret == False: 
+    if ret == False: #Detiene el proceso cuando no hay imagen
         print("No se detecta imágen")
         break
     elif cv2.waitKey(1) & 0xFF == ord('s'): #Detener el proceso con s en 64 bits
         break
-    cv2.imshow('Video', frame)
+    #cv2.imshow('Video', frame)
     main(frame)
     
 cap.release()
-
-#start_time = time()
-#main()
-#final_time = time()-start_time
-#print("Tiempo: %.10f seg" %final_time)
-
 cv2.destroyAllWindows()
